29f15fe0c600316d8abc0d3b18eb98d2a5a33c45
Craig Allen <callen7@hotmail.co.uk>
2024-06-12 22:10:34 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsmember() {
+    var member = document.getElementById('member');
+    var membervalue = member.options[member.selectedIndex].value;
+    if (membervalue == 'yes') {
+        document.getElementById('skill').style.display = 'block';
+    } else {
+        document.getElementById('skill').style.display = 'none';
+    }
+}
